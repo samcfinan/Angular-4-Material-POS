8d519e8cf0e2b70dd10d1adf70af826a98aac90b
Sam C. Finan <samcfinan@gmail.com>
2017-10-10 16:08:29 -0400

    Created component scaffolding using Angular CLI

---
+++ src/app/ticket/ticket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket',
+  templateUrl: './ticket.component.html',
+  styleUrls: ['./ticket.component.scss']
+})
+export class TicketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
