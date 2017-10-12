0415471ea69bc601643add9990dd2c8c162913b6
Sam C. Finan <samcfinan@gmail.com>
2017-10-12 13:53:41 -0400

    Added add and remove methods to the POS ticket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatGridListModule} from '@angular/material';
+import {MatTableModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -22,7 +23,8 @@ import { TicketComponent } from './ticket/ticket.component';
   ],
   imports: [
     BrowserModule,
-    MatGridListModule
+    MatGridListModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ticket/ticket.component.ts
+++ src/app/ticket/ticket.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Item {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
 @Component({
   selector: 'app-ticket',
   templateUrl: './ticket.component.html',
@@ -7,9 +14,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketComponent implements OnInit {
 
+  items = ITEMS;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  // Add item to ticket.
+  addItem(item: Item) {
+    // If the item already exists, add 1 to quantity
+    if (this.items.includes(item)) {
+      this.items[this.items.indexOf(item)].quantity += 1;
+    } else {
+      this.items.push(item);
+    }
+  }
+
+  removeItem(item: Item) {
+    // Check if item is in array
+    if (this.items.includes(item)) {
+      // Splice the element out of the array
+      const index = this.items.indexOf(item);
+      if (index > -1) {
+        this.items.splice(index, 1);
+      }
+    }
+  }
+
 }
+
+
+
+const ITEMS = [
+  {id: 1, name: 'Coffee', price: 2.00, quantity: 1},
+  {id: 2, name: 'Americano', price: 3.00, quantity: 1},
+  {id: 3, name: 'Cappuccino', price: 3.25, quantity: 2}
+];
