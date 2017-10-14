c07b30406e342fc8b8a08477db724e042bf6a5ee
Sam C. Finan <samcfinan@gmail.com>
2017-10-14 11:00:58 -0400

    Removed demo content, cursor is now pointer on hover

--- src/app/pos.service.ts
+++ src/app/pos.service.ts
@@ -19,8 +19,4 @@ export class PosService {
 
 // Demo content
 const TICKET: Item[] = [
-  {id: 1, name: 'Drip Coffee', price: 2.25, img: '../../assets/menu-images/drip-coffee.jpg', quantity: 1},
-  {id: 2, name: 'Americano', price: 3.15, img: '../../assets/menu-images/americano.png', quantity: 1},
-  {id: 3, name: 'Cappuccino', price: 3.50, img: '../../assets/menu-images/cappuccino.jpg', quantity: 1},
-  {id: 4, name: 'Cortado', price: 3.50, img: '../../assets/menu-images/cortado.jpg', quantity: 1},
 ];
