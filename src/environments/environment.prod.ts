90bba00f87f3da3e444c677312fcf526bea5dac2
Sam C. Finan <samcfinan@gmail.com>
2017-10-16 14:52:18 -0400

    Fixed date in history

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyASE-ZPKkrdKfW8K1skuUUacJG35-_J9oE',
+    authDomain: 'angularpos.firebaseapp.com',
+    databaseURL: 'https://angularpos.firebaseio.com',
+    projectId: 'angularpos',
+    storageBucket: 'angularpos.appspot.com',
+    messagingSenderId: '909339433794'
+  }
 };
