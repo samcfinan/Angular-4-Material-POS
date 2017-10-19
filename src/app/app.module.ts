8e8ba31f22fee2755adcaac8c5199c606cb1e503
Sam C. Finan <samcfinan@gmail.com>
2017-10-19 18:46:59 -0400

    Added to transaction history page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,7 @@ import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './core/app-routing.module';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatIconModule} from '@angular/material';
-import {MatButtonModule} from '@angular/material';
-import {MatTabsModule} from '@angular/material';
-import {MatCardModule} from '@angular/material';
-import {MatGridListModule} from '@angular/material';
-import {MatDialogModule} from '@angular/material';
+import {MatIconModule, MatButtonModule, MatTabsModule, MatCardModule, MatDialogModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -21,6 +16,7 @@ import { TicketComponent } from './ticket/ticket.component';
 import { PosService } from './pos.service';
 
 import { environment } from '../environments/environment';
+
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
@@ -55,7 +51,6 @@ export const firebaseConfig = environment.firebaseConfig;
     MatTabsModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatGridListModule,
     MatDialogModule,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireAuthModule,
--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from '../database.service';
+import { Order, Item } from '../item';
 
 @Component({
   selector: 'app-transactions',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionsComponent implements OnInit {
 
-  constructor() { }
+  history: any;
+
+  constructor(private db: DatabaseService) { }
 
   ngOnInit() {
+    this.db.getTicketList(5).subscribe(data => this.history = data);
   }
 
 }
