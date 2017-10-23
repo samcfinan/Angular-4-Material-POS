e88cb6f25e93c52bc0466374dba7abaf227e799a
Sam C. Finan <samcfinan@gmail.com>
2017-10-23 11:05:31 -0400

    Transaction history now opens modal on click, but not when delete is hovered

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DatabaseService } from '../database.service';
 import { Order, Item } from '../item';
+import { LineItemModalComponent } from '../history/line-item-modal/line-item-modal.component';
+
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
   selector: 'app-transactions',
@@ -11,10 +14,20 @@ export class TransactionsComponent implements OnInit {
 
   history: any;
 
-  constructor(private db: DatabaseService) { }
+  constructor(private db: DatabaseService, public dialog: MatDialog) { }
 
   ngOnInit() {
     this.db.getOrderFeed(25).subscribe(data => this.history = data);
+  }
+
+  openDialog(lineItem: Order): void {
+    const dialogRef = this.dialog.open(LineItemModalComponent, {
+      width: '300px',
+      data: { numItems: lineItem.cartNumItems,
+        orderNumber: lineItem.orderNumber,
+        items: lineItem.items,
+        total: lineItem.cartTotal }
+    });
   }
 
   getNextPage() {
