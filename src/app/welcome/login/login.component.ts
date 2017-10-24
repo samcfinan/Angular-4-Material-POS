d7226eca1bad2166ff2e7b86b1ba9dfb6ca0f22b
Sam C. Finan <samcfinan@gmail.com>
2017-10-24 15:24:55 -0400

    Styled Login page

--- src/app/welcome/login/login.component.ts
+++ src/app/welcome/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../core/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
   }
