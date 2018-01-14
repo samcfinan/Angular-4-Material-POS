54558462cb04b0e3499523abf32b7a3b9ee66771
Sam C. Finan <samcfinan@gmail.com>
2018-01-14 00:34:56 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,6 @@ export const environment = {
   production: false,
   // Initialize Firebase
   firebaseConfig: {
-    apiKey: 'AIzaSyASE-ZPKkrdKfW8K1skuUUacJG35-_J9oE',
     authDomain: 'angularpos.firebaseapp.com',
     databaseURL: 'https://angularpos.firebaseio.com',
     projectId: 'angularpos',
